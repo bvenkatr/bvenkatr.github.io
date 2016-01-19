98c319b58a44c2e3fd78d5e1478354e23f26a3ec
Venkat <venkat.jaa@gmail.com>
2016-01-19 23:09:52 +0530

    Add my profiles

---
+++ myProfile.js
@@ -0,0 +1,11 @@
+var myprofile = {
+	"github": "https://github.com/bvenkatr",
+	"codepen": "http://codepen.io/bvenkatr/",
+	"jsfiddle": "https://jsfiddle.net/user/bvenkatr/fiddles/",
+	"myblog": "http://bvenkatr.blogspot.in/",
+	"linkedin": "https://www.linkedin.com/nhome/",
+	"facebook": "https://www.facebook.com/venkateswarareddy.baddigam",
+	"gitbooks": "https://www.gitbook.com/@bvenkatr/dashboard"
+};
+
+export const myprofile = myprofile;
